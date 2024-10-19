8ba6788c4f7e15b4b6c085f228c238214d6226c6
jdusil <jdusil@veracode.com>
2024-10-19 19:42:46 +0200

    lesson_11

--- lesson_11_a/vite/vite-project/src/components/UserList/UserList.tsx
+++ lesson_11_a/vite/vite-project/src/components/UserList/UserList.tsx
@@ -1,87 +1,97 @@
-const people = [
-    {
-        name: 'Leslie Alexander',
-        email: 'leslie.alexander@example.com',
-        role: 'Co-Founder / CEO',
-        imageUrl:
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        lastSeen: '3h ago',
-        lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-        name: 'Michael Foster',
-        email: 'michael.foster@example.com',
-        role: 'Co-Founder / CTO',
-        imageUrl:
-            'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        lastSeen: '3h ago',
-        lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-        name: 'Dries Vincent',
-        email: 'dries.vincent@example.com',
-        role: 'Business Relations',
-        imageUrl:
-            'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        lastSeen: null,
-    },
-    {
-        name: 'Lindsay Walton',
-        email: 'lindsay.walton@example.com',
-        role: 'Front-end Developer',
-        imageUrl:
-            'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        lastSeen: '3h ago',
-        lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-        name: 'Courtney Henry',
-        email: 'courtney.henry@example.com',
-        role: 'Designer',
-        imageUrl:
-            'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        lastSeen: '3h ago',
-        lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-        name: 'Tom Cook',
-        email: 'tom.cook@example.com',
-        role: 'Director of Product',
-        imageUrl:
-            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        lastSeen: null,
-    },
-]
+import { useEffect, useState } from "react";
+
+/*  
+Cíl: Vytvořit React komponentu, která načítá a zobrazuje seznam uživatelů z API
+pomocí hooku useEffect.
+
+● Nastavení projektu:
+    ○ Vytvořte nový React projekt pomocí Create React App:
+        npx create - react - app data - fetching - hw--template typescript
+    ○ Přejděte do nově vytvořené složky:
+        cd data - fetching - hw
+● Vytvoření komponenty:
+    ○ V src / components složce vytvořte nový soubor s názvem UserList.tsx
+    ○ V tomto souboru vytvořte funkční komponentu UserList
+    ○ Nezapomeňte vytvořit barrel file index.ts, abychom neměli nehezké importy
+● Pomocí useEffect a useState načtěte seznam uživatelů z mockAPI
+    ○ https://jsonplaceholder.typicode.com/users
+● Renderování komponenty:
+    ○ Zobrazte zprávu "Načítání..." když se data načítají(použijte state)
+    ○ Po načtení dat zobrazte seznam jmen uživatelů
+    ○ Pro každého uživatele zobrazte jméno, email a název společnosti
+● Nezapomeňte komponentu vyrenderovat v App
+● Implementujte zachycení a zobrazení chyb:
+    ○ Přidejte nový state pro ukládání chybových zpráv
+    ○ Zobrazte chybovou zprávu uživateli, pokud načtení dat selže
+*/
+
+const url = 'https://jsonplaceholder.typicode.com/users';
+
+export type User = {
+    id: number
+    name: string
+    username: string
+    email: string
+    address?: {
+        street: string
+        suite: string
+        city: string
+        zipcode: string
+        geo: {
+            lat: string
+            lng: string
+        }
+    }
+    phone: string
+    website: string
+    company?: {
+        name: string
+        catchPhrase: string
+        bs: string
+    }
+}
 
 export const UserList = () => {
+    const [users, setUsers] = useState<User[]>();
+    const [error, setError] = useState<string>();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(url);
+            if (!response.ok) {
+                setError('Data se nepodařilo načíst');
+                return;
+            }
+            const data = await response.json();
+            setUsers(data);
+        };
+        fetchData();
+    }, []);
+
+    if (error) {
+        return (
+            <div>
+                <p>{error}</p>
+            </div>
+        );
+    }
+
+    if (!users) {
+        return <p>Načítání...</p>;
+    }
     return (
         <ul role="list" className="divide-y divide-gray-100">
-            {people.map((person) => (
-                <li key={person.email} className="flex justify-between gap-x-6 py-5">
+            {users?.map((user) => (
+                <li key={user.email} className="flex justify-between gap-x-6 py-5">
                     <div className="flex min-w-0 gap-x-4">
-                        <img alt="" src={person.imageUrl} className="h-12 w-12 flex-none rounded-full bg-gray-50" />
                         <div className="min-w-0 flex-auto">
-                            <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
-                            <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
+                            <p className="text-sm font-semibold leading-6 text-gray-900">{user.name}</p>
+                            <p className="mt-1 truncate text-xs leading-5 text-gray-500">Email: {user.email}</p>
+                            <p className="mt-1 truncate text-xs leading-5 text-gray-500">Company: {user.company?.name}</p>
                         </div>
-                    </div>
-                    <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                        <p className="text-sm leading-6 text-gray-900">{person.role}</p>
-                        {person.lastSeen ? (
-                            <p className="mt-1 text-xs leading-5 text-gray-500">
-                                Last seen <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>
-                            </p>
-                        ) : (
-                            <div className="mt-1 flex items-center gap-x-1.5">
-                                <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                                    <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
-                                </div>
-                                <p className="text-xs leading-5 text-gray-500">Online</p>
-                            </div>
-                        )}
                     </div>
                 </li>
             ))}
         </ul>
     )
-}
+};
