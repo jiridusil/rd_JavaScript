228bff5eaf9115ca1a80c5fd509d3f6c0b62fa2f
jdusil <jdusil@veracode.com>
2024-10-29 21:02:37 +0100

    White color for Add Product text

--- react_ts/src/components/AddProductForm/AddProductForm.tsx
+++ react_ts/src/components/AddProductForm/AddProductForm.tsx
@@ -63,7 +63,7 @@ export const AddProductForm = ({ updateProducts }: Props) => {
                 <form onSubmit={handleSubmit}>
                     <div className="space-y-12">
                         <div className="border-b border-gray-900/10 pb-12">
-                            <h2 className="text-base font-semibold leading-7 text-gray-900">Add Product</h2>
+                            <h2 className={`text-base font-semibold leading-7 text-gray-900 ${theme === 'dark' && 'text-white'}`}>Add Product</h2>
                             <p className="mt-1 text-sm leading-6 text-gray-600"></p>
                             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                                 <div className="sm:col-span-4">
